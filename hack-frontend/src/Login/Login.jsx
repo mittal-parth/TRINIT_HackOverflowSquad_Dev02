6ea31aedf58bc61696baf0bd2f0f05941da62304
anas_aijaz <cjphonehomec@gmail.com>
2022-01-29 17:12:22 +0530

    added frontend part

---
+++ hack-frontend/src/Login/Login.jsx
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import TopBar from '../components/TopBar/TopBar';
+
+const Login = () => {
+  return (
+    <>
+    <TopBar/>
+    <Box>
+      asnasd
+    </Box>
+    </>
+  );
+};
+
+export default Login;
